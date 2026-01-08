56e6c33266aa9cffea281347583b997ff6df2dcd
Soumojit777 <poddarsoumojit777@gmail.com>
2026-01-08 21:46:27 +0530

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,18 @@
 import express from "express";
 import path from "path";
-import cors from "cors";
-import { serve } from "inngest/express";
-import { clerkMiddleware } from "@clerk/express";
 
 import { ENV } from "./lib/env.js";
-import { connectDB } from "./lib/db.js";
-import { inngest, functions } from "./lib/inngest.js";
-
-// import chatRoutes from "./routes/chatRoutes.js";
-// import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
-// middleware
-// app.use(express.json());
-// credentials:true meaning?? => server allows a browser to include cookies on request
-// app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-// app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
-
-// app.use("/api/inngest", serve({ client: inngest, functions }));
-// app.use("/api/chat", chatRoutes);
-// app.use("/api/sessions", sessionRoutes);
-
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
+});
+
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "this is the books endpoint" });
 });
 
 // make our app ready for deployment
@@ -38,13 +24,4 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
-  } catch (error) {
-    console.error("💥 Error starting the server", error);
-  }
-};
-
-startServer();
+app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
